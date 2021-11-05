545f7796b29dc9cf10070295f750592b51d23530
unknown <chesebrojared@gmail.com>
2021-11-04 17:05:20 -0700

    fix commit

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+//adds path to connect public folder
+const path = require('path');
+// handlebars setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// setting handlebars as the engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// this is what connects the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
